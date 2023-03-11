105c8d88dd1d4d3cb4689f56b9c862ce153007e2
Enciusan <enciulescu.alexandru@gmail.com>
2023-03-12 01:38:13 +0200

    create campaign layout WIP on SC call

--- client/src/pages/Space.tsx
+++ client/src/pages/Space.tsx
@@ -1,5 +1,20 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Box, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useGetAccountInfo } from "@multiversx/sdk-dapp/hooks";
 import React, { useEffect, useState } from "react";
 import { Contract } from "sdk/contract.sdk";
@@ -10,6 +25,8 @@ export const Space = () => {
   const [ceva, setCeva] = useState<Campaign[]>([]);
   const [spaceId, setSpaceId] = useState("");
   const [nftImage, setNftImage] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [checkedItems, setCheckedItems] = useState(true);
 
   const buildNftId = (collectionId: string, nonce: number) => {
     let hexnonce = nonce?.toString(16);
@@ -37,9 +54,12 @@ export const Space = () => {
       <span className="font-medium pl-10 text-black/60 text-sm">Space: {spaceId}</span>
 
       <div className="flex flex-row justify-between">
-        <span className="font-medium pl-10 text-black/80 mt-2">Your active campains</span>
-        <div>
-          <AddIcon className="mr-10">ceva</AddIcon>
+        <span className="flex flex-row justify-center items-center font-medium pl-10 text-black/80 mt-2">Your active campains</span>
+        <div className="flex flex-row justify-center items-center">
+          <Button className="!my-4 !w-full !mr-10 !bg-teal-300 hover:!bg-teal-400 hover:scale-105 rounded-lg font-medium text-center" onClick={onOpen}>
+            <Text>Create</Text>
+            <AddIcon className="mx-2" boxSize={3} />
+          </Button>
         </div>
       </div>
       {ceva.map((campaigns, index) => {
@@ -60,7 +80,39 @@ export const Space = () => {
           </div>
         );
       })}
-      {/*{ceva.spaceId}*/}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <div className="flex flex-row">
+              <div className="my-auto w-7 h-7 bg-teal-300 rounded-md">
+                <AddIcon className="-mt-2 mx-2" boxSize={3} />
+              </div>
+              <p className="ml-2">Create a new campaign</p>
+            </div>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody className="flex flex-col">
+            <Input className="my-2" placeholder="Name" />
+            <Input className="my-2" placeholder="Image URL" />
+            <Input className="my-2" placeholder="Attributes" />
+            <Input className="my-2" placeholder="Claim amount per user" />
+            <Checkbox className="my-2 ml-0.5" defaultChecked onChange={(e) => setCheckedItems(e.target.checked)}>
+              Redeem via KYC?
+            </Checkbox>
+            <Checkbox className="my-2 ml-0.5" defaultChecked isDisabled={checkedItems}>
+              Campaign require whitelist?
+            </Checkbox>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button className="!bg-teal-300">Create</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
