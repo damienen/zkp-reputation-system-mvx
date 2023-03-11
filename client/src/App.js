3f9b707b0446d30fceb276dc25350fc87aa5ea18
Damian <damian.ovidiu27@gmail.com>
2023-03-11 16:55:45 +0200

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
-import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+import { NotificationModal, SignTransactionsModals, TransactionsToastList } from "@multiversx/sdk-dapp/UI";
 import { DappProvider } from "@multiversx/sdk-dapp/wrappers";
-import {
-  NotificationModal,
-  SignTransactionsModals,
-  TransactionsToastList,
-} from "@multiversx/sdk-dapp/UI";
-import { PageNotFound } from "./pages/PageNotFound";
-import About from "./pages/About";
-import Space from "./pages/Space";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import { PageNotFound } from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -24,8 +19,7 @@ function App() {
           name: "customConfig",
           apiTimeout: 6000,
           walletConnectV2ProjectId: "a5a07b89bf08093c43938c81a8f1a937",
-        }}
-      >
+        }}>
         <TransactionsToastList />
         <NotificationModal />
         <SignTransactionsModals className="custom-class-for-modals" />
