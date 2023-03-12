187843491c5301b7aed9ff3a8998126c6afa7151
Enciusan <enciulescu.alexandru@gmail.com>
2023-03-12 03:45:00 +0200

    create campaign working

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,23 +1,11 @@
 import { Flex } from "@chakra-ui/react";
+import { useGetAccountInfo } from "@multiversx/sdk-dapp/hooks";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-// const spaces = [
-//   {
-//     space_id: "QRDS23",
-//     name: "Ceva",
-//   },
-//   {
-//     space_id: "ASDW231",
-//     name: "Altceva",
-//   },
-//   {
-//     space_id: "DAWD8231",
-//     name: "Test",
-//   },
-// ];
-
 export const Home = () => {
+  const { address } = useGetAccountInfo();
+  const isLoggedIn = Boolean(address);
   return (
     <Flex className="w-full flex-col">
       <h1 className="flex w-full justify-center py-3 text-xl font-semibold">Home</h1>
@@ -30,12 +18,16 @@ export const Home = () => {
       {/*  ))}*/}
       {/*</Box>*/}
       <div className="flex w-full h-[75dvh] justify-center items-center">
-        <NavLink to={"/reputation"} className="!bg-teal-300 hover:!bg-teal-400 hover:scale-105 mx-2 px-4 py-2 rounded-lg font-medium">
-          REPUTATION GIVER
-        </NavLink>
-        <NavLink to={"/collector"} className="!bg-teal-300 hover:!bg-teal-400 hover:scale-105 mx-2 px-4 py-2 rounded-lg font-medium">
-          COLLECTOR
-        </NavLink>
+        {isLoggedIn && (
+          <>
+            <NavLink to={"/reputation"} className="!bg-teal-300 hover:!bg-teal-400 hover:scale-105 mx-2 px-4 py-2 rounded-lg font-medium">
+              REPUTATION GIVER
+            </NavLink>
+            <NavLink to={"/collector"} className="!bg-teal-300 hover:!bg-teal-400 hover:scale-105 mx-2 px-4 py-2 rounded-lg font-medium">
+              COLLECTOR
+            </NavLink>
+          </>
+        )}
       </div>
     </Flex>
   );
