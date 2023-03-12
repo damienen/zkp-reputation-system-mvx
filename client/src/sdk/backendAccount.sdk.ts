b509a0f212a2f73334aa3b8f7be42fe5f9af8e0c
Damian <damian.ovidiu27@gmail.com>
2023-03-12 03:34:09 +0200

    update backendAccount

--- client/src/sdk/backendAccount.sdk.ts
+++ client/src/sdk/backendAccount.sdk.ts
@@ -1,29 +1,22 @@
 /**
-* This is an auto generated code. This code should not be modified since the file can be overwriten 
-* if new genezio commands are executed.
-*/
-     
-import { Remote } from "./remote"
+ * This is an auto generated code. This code should not be modified since the file can be overwriten
+ * if new genezio commands are executed.
+ */
 
-export const networkProvider = new ProxyNetworkProvider(
-    "https://devnet-gateway.multiversx.com"
-  );;
+import { Remote } from "./remote";
 
-
+export const networkProvider = new ProxyNetworkProvider("https://devnet-gateway.multiversx.com");
 
 export class BackendAccount {
-    static remote = new Remote("http://127.0.0.1:8083/BackendAccount")
+  static remote = new Remote("http://127.0.0.1:8083/BackendAccount");
 
-    static async sync(password: string): Promise<any> {
-        return await BackendAccount.remote.call("BackendAccount.sync", password)  
+  static async sync(password: string): Promise<any> {
+    return await BackendAccount.remote.call("BackendAccount.sync", password);
   }
 
   static async sendTransaction(tokenIdentifier: string, nonce: number, address: string, password: string): Promise<any> {
-        return await BackendAccount.remote.call("BackendAccount.sendTransaction", tokenIdentifier, nonce, address, password)  
+    return await BackendAccount.remote.call("BackendAccount.sendTransaction", tokenIdentifier, nonce, address, password);
   }
-
-  
-
 }
 
 export { Remote };
