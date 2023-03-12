f50f8e12720693ec69de894e3fcfd1eddcb3c1bb
Damian <damian.ovidiu27@gmail.com>
2023-03-12 04:28:07 +0200

    genezio kyc

--- server/src/kyc.ts
+++ server/src/kyc.ts
@@ -2,15 +2,22 @@ import { BackendAccount } from "./backendAccount";
 import { walletPassword } from "./constants";
 export class KycWebhook {
   kycWebhook(request: any) {
-    const key = request.body.key;
-    const password = walletPassword;
-    const backend = new BackendAccount();
-    if (password) {
-      backend.checkKycKey(key, password);
+    if (request.type === "POST") {
+      const key = request.body.key;
+      const password = walletPassword;
+      const backend = new BackendAccount();
+      if (password) {
+        backend.checkKycKey(key, password);
+      }
+      return {
+        statusCode: "200",
+        statusDescription: "Ok",
+      };
+    } else {
+      return {
+        statusCode: "405",
+        statusDescription: "Method Not Allowed",
+      };
     }
-    return {
-      statusCode: "200",
-      statusDescription: "Ok",
-    };
   }
 }
